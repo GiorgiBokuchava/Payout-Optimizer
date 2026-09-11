f4ddb37cbb65edb978c4592ae9ed3b4d44c4b683
GiorgiBokuchava <1giorgibokuchava@gmail.com>
2026-09-11 14:12:29 +0400

    initialize Spring Boot project

---
+++ src/main/java/com/example/kursi_ge_payout_optimizer/KursiGePayoutOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursi_ge_payout_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursiGePayoutOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursiGePayoutOptimizerApplication.class, args);
+	}
+
+}
